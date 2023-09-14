bd4ba324ed787c04598a6016be6a5a7c4cd6008d
Sooraj-s-98 <52352285+Sooraj-s-98@users.noreply.github.com>
2023-09-14 17:37:40 +0530

    2 files modified

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import { createI18n } from 'vue-i18n';
 import App from './App.vue';
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
 
 import en from './locales/en.json';
 import ja from './locales/ja.json';
@@ -14,4 +15,6 @@ const i18n = createI18n({
   },
 });
 
-createApp(App).use(i18n).mount('#app');
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+createApp(App).use(i18n).use(BootstrapVue).use(BootstrapVueIcons).mount('#app');
